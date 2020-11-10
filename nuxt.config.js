a117e73fc916ca4a30c05eef4f63ad9a1958af8a
__angel__ <lasantiagoguerrero@gmail.com>
2020-11-09 20:06:19 -0600

    Sirviendo archivo CV pdf

--- nuxt.config.js
+++ nuxt.config.js
@@ -210,6 +210,17 @@ export default {
         tailwindcss: {},
         autoprefixer: {}
       }
+    },
+
+    // Load pdf file
+    extend (config) {
+      // Find the rule which contains a assets file extension
+      const assetsLoader = config.module.rules.find(rule => rule.test.test('.png'))
+
+      // Overwrite the test regex and add `pdf`
+      assetsLoader.test = /\.(png|jpe?g|gif|svg|webp|pdf)$/i
+
+      return config
     }
   }
 }
